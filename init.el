08692cb4b6df7867df4de4281c2ea29af14da552
Topher Brown <chris.brown@farsounder.com>
2010-11-01 14:20:52 -0400

    disabling autosave- possibly will stop #file.ext# from showing up

--- init.el
+++ init.el
@@ -38,6 +38,7 @@
 
 ;; misc settings
 (setq-default backup-inhibited t) ;; no backups
+(setq auto-save-default nil) ;; no autosave
 (global-auto-revert-mode) ;; automatically revert unmodified buffers that changed
 (fset 'yes-or-no-p 'y-or-n-p) ;; make all yes/no questions y/n
 (desktop-save-mode 1) ;; restore emacs state on startup
