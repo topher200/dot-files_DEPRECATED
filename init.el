5beee2346a1f5a3f92b946f58136816ef3dad0fd
Topher <topher200@gmail.com>
2010-11-01 23:58:50 -0400

    python-check and c++ compile with <f7>. may not work for c++

--- init.el
+++ init.el
@@ -24,8 +24,10 @@
 (global-set-key (kbd "M-=") 'balance-windows)
 
 ;; programming keybindings
-(global-set-key (kbd "<f7>") '(lambda () (interactive) (compile compile-command)))
 (global-set-key(kbd "C-s") 'save-buffer)
+;; run the check/compile for the current language
+(define-key python-mode-map (kbd "<f7>") 'python-check)
+(define-key c++-mode-map (kbd "<f7>") 'delete-window)
 
 ;; movement control
 (global-set-key(kbd "C-j") 'scroll-up)
