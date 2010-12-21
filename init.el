66625cfb46e8aefa04a541dba9796c26f4894359
Topher Brown <chris.brown@farsounder.com>
2010-12-21 13:14:08 -0500

    adding custom.el to my init.el

--- init.el
+++ init.el
@@ -1,6 +1,10 @@
 ;; path update
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp"))
 
+;; Variables configured via 'customize' interface
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 ;; startup with vimpulse
 (require 'vimpulse)
 (setq viper-mode t)
