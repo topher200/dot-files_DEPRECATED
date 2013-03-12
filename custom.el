ef6e4ef503e44f392e9fcd733712409ae4dc30f9
Topher Brown <chris.brown@farsounder.com>
2013-03-12 17:31:13 -0400

    use scroll-conservatively to match vim's "G" behaviour

--- custom.el
+++ custom.el
@@ -1,18 +1,19 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(fill-column 78)
  '(hippie-expand-try-functions-list (quote (try-expand-dabbrev try-expand-dabbrev-visible try-expand-dabbrev-from-kill try-complete-file-name-partially try-expand-dabbrev-all-buffers try-complete-lisp-symbol-partially try-expand-line)))
  '(ido-auto-merge-work-directories-length -1)
  '(ido-enable-tramp-completion nil)
+ '(scroll-conservatively 1000000)
  '(tab-width 2)
  '(tramp-mode nil)
  '(warning-suppress-types (quote (undo discard-info))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
