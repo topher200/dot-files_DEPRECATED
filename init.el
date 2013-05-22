047fa41fc84d926d891ad60f5b33b457f570e76b
Topher Brown <topher200@gmail.com>
2013-05-21 22:27:38 -0400

    init: add html mode syntax table

--- init.el
+++ init.el
@@ -164,13 +164,15 @@
 (add-hook 'ruby-mode-hook
           '(lambda ()
              (modify-syntax-entry ?_ "w" ruby-mode-syntax-table)))
-
 (add-hook 'c-mode-common-hook
           '(lambda ()
              (modify-syntax-entry ?_ "w" c-mode-syntax-table)))
 (add-hook 'c-mode-common-hook
           '(lambda ()
              (modify-syntax-entry ?_ "w" c++-mode-syntax-table)))
+(add-hook 'html-mode-hook
+          '(lambda ()
+             (modify-syntax-entry ?_ "w" html-mode-syntax-table)))
 
 ;; evan functions: google()
 (require 'evan-functions)
