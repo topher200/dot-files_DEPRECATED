d53289ebe634a4f14d98313fa9f6d6b673e5b097
Topher Brown <chris.brown@farsounder.com>
2013-06-06 16:55:51 -0400

    custom: new FS standard - 80 columns for everything

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fill-column 78)
+ '(fill-column 80)
  '(hippie-expand-try-functions-list (quote (try-expand-dabbrev try-expand-dabbrev-visible try-expand-dabbrev-from-kill try-complete-file-name-partially try-expand-dabbrev-all-buffers try-complete-lisp-symbol-partially try-expand-line)))
  '(ido-auto-merge-work-directories-length -1)
  '(ido-enable-tramp-completion nil)
