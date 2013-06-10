4feba6491ae3f59eb445f9ff1ead9e20a60faa8d
Topher Brown <chris.brown@farsounder.com>
2013-06-10 15:38:11 -0400

    custom: updates for whitespace mode

--- custom.el
+++ custom.el
@@ -11,10 +11,11 @@
  '(scroll-conservatively 1000000)
  '(tab-width 2)
  '(tramp-mode nil)
- '(warning-suppress-types (quote (undo discard-info))))
+ '(warning-suppress-types (quote (undo discard-info)))
+ '(whitespace-display-mappings (quote ((space-mark 160 [164] [95]) (space-mark 2208 [2212] [95]) (space-mark 2336 [2340] [95]) (space-mark 3616 [3620] [95]) (space-mark 3872 [3876] [95]) (tab-mark 9 [187 9] [92 9])))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(whitespace-space ((t nil))))
