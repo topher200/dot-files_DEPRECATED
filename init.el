968bf81780540a0411bc0fd81d4416fc5a9e2c7e
Topher Brown <chris.brown@farsounder.com>
2014-02-28 21:39:19 -0500

    'gofmt-before-save

--- init.el
+++ init.el
@@ -159,6 +159,7 @@
 
 ;; go-mode
 (add-to-list 'auto-mode-alist '("\\.go$" . go-mode))
+(add-hook 'before-save-hook 'gofmt-before-save)
 
 ;; lua mode
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
